f3a8dc69e147089d2d8335a278c06cbf3c1c99f6
CarinaChenot <mllecarinach@gmail.com>
2017-03-22 17:28:39 +0100

    :sparkles: Added ES6 babel support

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,22 @@ const gulp         = require('gulp')
 const plumber      = require('gulp-plumber')
 const rename       = require('gulp-rename')
 const notify       = require('gulp-notify')
+const minify       = require('gulp-minify')
 const connect      = require('gulp-connect')
 const autoprefixer = require('gulp-autoprefixer')
 const sourcemaps   = require('gulp-sourcemaps')
-const concat       = require('gulp-concat')
-const uglify       = require('gulp-uglify')
-const cleanCSS     = require('gulp-clean-css')
 const sass         = require('gulp-sass')
+const babel        = require('gulp-babel')
+
+
+let config = {
+    'src' : 'src/',
+    'dist': 'dist/'
+}
+
 
 // Connect task
-gulp.task('connect', function() {
+gulp.task('connect', () => {
     connect.server({
         root: 'dist',
         livereload: true
@@ -19,8 +25,8 @@ gulp.task('connect', function() {
 })
 
 // CSS task
-gulp.task('sass', function () {
-    return gulp.src('src/sass/**/*.scss')
+gulp.task('sass', () => {
+    return gulp.src(config.src + 'sass/*.scss')
         .pipe(plumber({errorHandler: notify.onError('SASS Error: <%= error.message %>')}))
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -32,33 +38,37 @@ gulp.task('sass', function () {
         .pipe(rename(function (path) {
             path.basename += ".min"
         }))
-        .pipe(gulp.dest('src/css/'))
+        .pipe(gulp.dest(config.dist + 'assets/css'))
         .pipe(connect.reload())
         .pipe(notify('SASS compiled: <%= file.relative %>'))
 })
 
 
 // JS task
-gulp.task('js', function(){
-  return gulp.src('src/js/**/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message)
-        this.emit('end')
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('src/js/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('src/js/'))
+gulp.task('js', () => {
+    return gulp.src(config.src + 'js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(minify({
+            ext:{
+                src:'.js',
+                min:'.min.js'
+          },
+          ignoreFiles: ['.min.js'],
+          noSource: false
+        }))
+        .pipe(gulp.dest(config.dist + 'assets/js'))
+        .pipe(connect.reload())
+        .pipe(notify('JS compiled: <%= file.relative %>'))
 })
 
 // Wath task
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.scss", ['sass'])
-    gulp.watch("src/js/**/*.js, '!js/**/*.min.js", ['js'])
+gulp.task('watch', () => {
+    gulp.watch(config.src + 'sass/**/*.scss', ['sass'])
+    gulp.watch(config.src + 'js/*.js', ['js'])
 })
 
-gulp.task('default', ['connect', 'watch'], function() {
+gulp.task('default', ['connect', 'watch'], () => {
 
 })
